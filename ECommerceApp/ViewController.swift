d69f4737bafe6818954e0e2edfb5ab69e7ef0b45
Terashima <yamanogomora@gmail.com>
2019-10-07 15:55:21 +0900

    ログインフローを先に作成するために、初期表示画面を一時的に変更する

--- ECommerceApp/ViewController.swift
+++ ECommerceApp/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    // 先にユーザーログインフローを作成するために、初期表示画面を一時的に変更する
+    // 後で削除する
+    override func viewDidAppear(_ animated: Bool) {
+        let storyboard = UIStoryboard(name: "LoginStoryboard", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "loginVC")
+        present(controller, animated: true, completion: nil)
     }
 
 
