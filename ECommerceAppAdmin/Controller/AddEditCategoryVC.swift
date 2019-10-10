708013638aef3a9239b5792fe865c9039036bfb4
Terashima <yamanogomora@gmail.com>
2019-10-10 10:35:01 +0900

    admin: admin用アプリのトップ画面とカテゴリ追加画面を作成

---
+++ ECommerceAppAdmin/Controller/AddEditCategoryVC.swift
@@ -0,0 +1,54 @@
+//
+//  AddEditCategoryVC.swift
+//  ECommerceAppAdmin
+//
+//  Created by 寺島 洋平 on 2019/10/10.
+//  Copyright © 2019 YoheiTerashima. All rights reserved.
+//
+
+import UIKit
+
+class AddEditCategoryVC: UIViewController {
+
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var categoryImg: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(imgTapped(_:)))
+        tap.numberOfTapsRequired = 1
+        categoryImg.isUserInteractionEnabled = true
+        categoryImg.addGestureRecognizer(tap)
+    }
+    
+    @objc func imgTapped(_ tap: UITapGestureRecognizer) {
+        launchImgPicker()
+    }
+
+    @IBAction func addCategoryClicked(_ sender: Any) {
+    }
+}
+
+extension AddEditCategoryVC : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func launchImgPicker() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.originalImage] as? UIImage else { return }
+        categoryImg.contentMode = .scaleToFill
+        categoryImg.image = image
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ ECommerceAppAdmin/Controller/AdminHomeVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ECommerceAppAdmin
+//
+//  Created by 寺島 洋平 on 2019/10/07.
+//  Copyright © 2019 YoheiTerashima. All rights reserved.
+//
+
+import UIKit
+
+class AdminHomeVC: HomeVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem?.isEnabled = false
+        let addCategoryBtn = UIBarButtonItem(title: "カテゴリ追加", style: .plain, target: self, action: #selector(addCategory))
+        navigationItem.rightBarButtonItem = addCategoryBtn
+    }
+    
+    @objc func addCategory() {
+        performSegue(withIdentifier: Segues.ToAddEditCategory, sender: self)
+    }
+
+
+}
+
