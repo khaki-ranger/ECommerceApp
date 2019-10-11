26b04f753546a5f9cc314e16f47480cdafcb83a1
Terashima <yamanogomora@gmail.com>
2019-10-11 18:03:55 +0900

    ユーザーを表現するデータモデル、User型を作成

---
+++ ECommerceApp/Model/User.swift
@@ -0,0 +1,45 @@
+//
+//  User.swift
+//  ECommerceApp
+//
+//  Created by 寺島 洋平 on 2019/10/11.
+//  Copyright © 2019 YoheiTerashima. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var id: String
+    var email: String
+    var username: String
+    var stripeId: String
+    
+    init(id: String = "",
+         email: String = "",
+         username: String = "",
+         stripeId: String = "") {
+        
+        self.id = id
+        self.email = email
+        self.username = username
+        self.stripeId = stripeId
+    }
+    
+    init(data: [String: Any]) {
+        id = data["id"] as? String ?? ""
+        email = data["email"] as? String ?? ""
+        username = data["username"] as? String ?? ""
+        stripeId = data["stripeId"] as? String ?? ""
+    }
+    
+    static func modelToData(user: User) -> [String: Any] {
+        let data : [String: Any] = [
+            "id" : user.id,
+            "email" : user.email,
+            "username" : user.username,
+            "stripeId" : user.stripeId
+        ]
+        
+        return data
+    }
+}
