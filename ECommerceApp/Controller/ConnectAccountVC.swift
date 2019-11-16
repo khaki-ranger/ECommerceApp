f9ee8562bcae83185fe505e8e137db49b8f4731a
Terashima <yamanogomora@gmail.com>
2019-11-16 19:30:36 +0900

    #13 Facebook認証 Facebookのデータを取得する処理を実装

--- ECommerceApp/Controller/ConnectAccountVC.swift
+++ ECommerceApp/Controller/ConnectAccountVC.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import Kingfisher
 
 class ConnectAccountVC: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var fbAvatar: RoundedImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = "アカウント連携"
+        navigationItem.title = "アカウント設定"
+        
+        fetchFbData()
+    }
+    
+    // ログインユーザーのFacebook登録情報を取得するためのメソッド
+    private func fetchFbData() {
+        let graphRequest = GraphRequest(graphPath: "me", parameters: ["fields": "id, email, name, picture.type(large)"], tokenString: AccessToken.current?.tokenString, version: Settings.defaultGraphAPIVersion, httpMethod: HTTPMethod.get)
+        
+        graphRequest.start { (connection, result, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+                return
+            }
+            
+            // 取得したFacebook登録情報から辞書型のデータを作成
+            guard let dictionary = result as? [String: Any] else { return }
+            guard let name = dictionary["name"] as? String,
+                let email = dictionary["email"] as? String else { return }
+            guard let pictureObject = dictionary["picture"] as? [String: Any],
+                let pictureData = pictureObject["data"] as? [String: Any],
+                let urlString = pictureData["url"] as? String else { return }
+            
+            self.userNameTxt.text = name
+            self.emailTxt.text = email
+            if let url = URL(string: urlString) {
+                let placeholder = UIImage(named: "placeholder")
+                let options : KingfisherOptionsInfo = [KingfisherOptionsInfoItem.transition(.fade(0.2))]
+                self.fbAvatar.kf.indicatorType = .activity
+                self.fbAvatar.kf.setImage(with: url, placeholder: placeholder, options: options)
+            }
+            
+        }
     }
     
 }
